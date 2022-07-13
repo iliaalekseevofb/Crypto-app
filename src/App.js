891756b72abdd1f8ec648ef6da55dac187e49cee
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-07-13 17:29:11 +0500

    Axios is set up, able to send requests and get responses from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import './App.css';
 
 const App = () => {
+
+  useEffect(() => {
+    Axios.get('https://api.coinstats.app/public/v1/coins?skip=0&limit=10&currency=EUR').then(
+      (response) => {
+        console.log(response.data)
+      })
+  }, [])
+
   return (
-    <div>App</div>
+    <div className='App'>
+      <div className='cryptoHeader'></div>
+      <div className='ctyptoDisplay'></div>
+    </div>
   )
 }
 

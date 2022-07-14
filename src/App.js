9c879c4d5af8400e29f08a0c3e6c566a6d892ea6
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-07-14 23:54:55 +0500

    Cards grid is done, no visible bugs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Axios from 'axios';
 import Navbar from './components/Navbar';
 import CoinCard from './components/CoinCard';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import './App.css';
 
 const App = () => {
@@ -11,7 +12,7 @@ const App = () => {
   const [searchWord, setSearchWord] = useState('');
 
   useEffect(() => {
-    Axios.get('https://api.coinstats.app/public/v1/coins?skip=0&currency=EUR').then(
+    Axios.get('https://api.coinstats.app/public/v1/coins?skip=0&limit=200&currency=EUR').then(
       (response) => {
         console.log(response.data);
         setListOfCoins(response.data.coins);
@@ -25,10 +26,30 @@ const App = () => {
   return (
     <div className='App'>
       <Navbar searchWordFunc={SearchWord => setSearchWord(SearchWord)}/>
-      <Box>
-        {filteredCoins.map((coin, index)=>{
-          return <CoinCard key={index} name={coin.name} icon={coin.icon} price={coin.price} symbol={coin.symbol} />
-        })}
+      <Box sx={{
+        padding: {xs: 4, md: 5},
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}
+      >
+        {filteredCoins.length < 0
+        ? <Box>
+            {filteredCoins.map((coin, index)=>{
+              return (
+                <CoinCard key={index} name={coin.name} icon={coin.icon} price={coin.price} symbol={coin.symbol} />
+            )})}
+          </Box>
+        : <Grid container rowSpacing={5} columnSpacing={{ xs: 3, sm: 4, md: 5 }}>
+            {filteredCoins.map((coin, index)=>{
+              return (
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                  <CoinCard key={index} name={coin.name} icon={coin.icon} price={coin.price} symbol={coin.symbol} />
+                </Grid>)
+            })}
+          </Grid>
+        }
       </Box>
     </div>
   )
--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 const CoinCard = ({name, icon, price, symbol}) => {
   return (
-    <div className='coin'>
-        <h1>Name: {name}</h1>
-        <img src={icon} alt='icon' />
-        <h3>Price: {price}</h3>
-        <h3>Symbol: {symbol}</h3>
-    </div>
+    <Card sx={{
+      minWidth: "250px",
+      backgroundColor: '#fafafa',
+      boxShadow: '0px 10px 21px 0px rgba(0, 0, 0, 0.21)'
+    }}>
+      <CardContent>
+        <Typography>Name: {name}</Typography>
+        <img className='icon' src={icon} alt='icon' />
+        <Typography>Price: {price}</Typography>
+        <Typography>Symbol: {symbol}</Typography>
+      </CardContent>
+    </Card>
   )
 }
 
